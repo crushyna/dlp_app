4b63e188bb8fcef47a6ddc0d317c4c889afa6056
crushyna <crushyna@gmail.com>
2020-08-31 12:31:02 +0200

    helpers.py cleaned

--- app/helpers/helpers.py
+++ app/helpers/helpers.py
@@ -16,27 +16,15 @@ class MainProgramHelper:
     def check_if_files_exist(filename: str, settings_file: str):
         if not os.path.isfile(os.path.join(GlobalSettings.acquisiton_folder, filename)):
             return f"File {filename} does not exist!"
-            # typer.echo(f"File {filename} does not exist!")
-            # raise typer.Exit()
 
         elif not os.path.isfile(os.path.join(GlobalSettings.localization_folder, settings_file)):
             return f"Settings file {settings_file} does not exist!"
-
-            # typer.echo(f"Settings file {settings_file} does not exist!")
-            # raise typer.Exit()
 
         else:
             return True
 
 
 class SaveTxtHelper:
-
-    @staticmethod
-    def to_fwf(df, fname):
-        content = tabulate(df.values.tolist(), list(df.columns), tablefmt="plain")
-        open(fname, "w").write(content)
-
-    pd.DataFrame.to_fwf = to_fwf
 
     @staticmethod
     def replace_string(filename: str, string: str, replacement: str):
