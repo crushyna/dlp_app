0843e807a7777b276242b2ae8774cf3c11d6c0d9
crushyna <crushyna@gmail.com>
2020-09-02 15:41:22 +0200

    fixed class functions for csv and excel importer

--- app/functions/csv_importer.py
+++ app/functions/csv_importer.py
@@ -7,9 +7,6 @@ from config.config_parser import LocalizationProcessingSettings
 from functions.processing_functions import ProcessingFunctions
 from helpers.helpers import GlobalSettings
 
-str_part_no = GlobalSettings.str_part_no
-str_price = GlobalSettings.str_price
-
 
 class CSVProcessingObject(LocalizationProcessingSettings, ProcessingFunctions):
 
@@ -19,35 +16,27 @@ class CSVProcessingObject(LocalizationProcessingSettings, ProcessingFunctions):
         self.settings_file = settings_file
         self.engine = None if engine is None else engine
 
-        self.initial_dataframe = self.read_csv_file(self.filename,
-                                                    self.header,
-                                                    self.names,
-                                                    self.delimiter,
-                                                    self.index_col,
-                                                    self.skiprows,
-                                                    self.columns_to_use,
-                                                    self.engine)
+        self.initial_dataframe = self.read_csv_file()
 
-    @staticmethod
-    def read_csv_file(filename: str, header, names, delimiter, index_col, skiprows: int, columns_to_use, engine: str):
-        logging.info(f"Reading CSV file: {filename}")
-        dataframe = pd.read_csv(os.path.join(GlobalSettings.acquisiton_folder, filename),
-                                header=None if header is None else header,
-                                names=None if names is None else names,
-                                delimiter=delimiter,
-                                index_col=None if index_col is None else index_col,
-                                usecols=tuple(columns_to_use),
-                                skiprows=skiprows,
+    def read_csv_file(self):
+        logging.info(f"Reading CSV file: {self.filename}")
+        dataframe = pd.read_csv(os.path.join(GlobalSettings.acquisiton_folder, self.filename),
+                                header=None if self.header is None else self.header,
+                                names=None if self.names is None else self.names,
+                                delimiter=self.delimiter,
+                                index_col=None if self.index_col is None else self.index_col,
+                                usecols=tuple(self.columns_to_use),
+                                skiprows=self.skiprows,
                                 dtype=str,
-                                engine=engine)
+                                engine=self.engine)
 
         # name columns properly
-        logging.debug(f"{filename}: naming columns")
-        dataframe.columns = [str_part_no, str_price]
+        logging.debug(f"{self.filename}: naming columns")
+        dataframe.columns = [self.columns_output_names[0], self.columns_output_names[1]]
 
         # change price strings to floats
-        logging.debug(f"{filename}: changing price to floats")
-        dataframe[str_price] = dataframe[str_price].str.replace(",", ".")
-        dataframe[str_price] = pd.to_numeric(dataframe[str_price])
+        logging.debug(f"{self.filename}: changing price to floats")
+        dataframe[self.columns_output_names[1]] = dataframe[self.columns_output_names[1]].str.replace(",", ".")
+        dataframe[self.columns_output_names[1]] = pd.to_numeric(dataframe[self.columns_output_names[1]])
 
         return dataframe
--- app/functions/excel_importer.py
+++ app/functions/excel_importer.py
@@ -1,5 +1,4 @@
 import logging
-
 import pandas as pd
 import os
 import xlrd
@@ -8,9 +7,6 @@ import pyxlsb
 from config.config_parser import LocalizationProcessingSettings
 from functions.processing_functions import ProcessingFunctions
 from helpers.helpers import GlobalSettings
-
-str_part_no = GlobalSettings.str_part_no
-str_price = GlobalSettings.str_price
 
 
 class ExcelProcessingObject(LocalizationProcessingSettings, ProcessingFunctions):
@@ -21,40 +17,34 @@ class ExcelProcessingObject(LocalizationProcessingSettings, ProcessingFunctions)
         self.settings_file = settings_file
         self.engine = None if engine is None else engine
 
-        self.initial_dataframe = self.read_excel_file(self.filename,
-                                                      self.header,
-                                                      self.names,
-                                                      self.index_col,
-                                                      self.skiprows,
-                                                      self.columns_to_use,
-                                                      self.engine)
+        self.initial_dataframe = self.read_excel_file()
 
-    @staticmethod
-    def read_excel_file(filename: str, header, names, index_col, skiprows: int, columns_to_use, engine: str):
-        logging.info(f"Reading Excel file: {filename}")
-        dataframe = pd.read_excel(os.path.join(GlobalSettings.acquisiton_folder, filename),
-                                  header=None if header is None else header,
-                                  names=None if names is None else names,
-                                  index_col=None if index_col is None else index_col,
-                                  usecols=tuple(columns_to_use),
+    def read_excel_file(self):
+        logging.info(f"Reading Excel file: {self.filename}")
+        dataframe = pd.read_excel(os.path.join(GlobalSettings.acquisiton_folder, self.filename),
+                                  header=None if self.header is None else self.header,
+                                  names=None if self.names is None else self.names,
+                                  index_col=None if self.index_col is None else self.index_col,
+                                  usecols=tuple(self.columns_to_use),
                                   convert_float=False,
-                                  skiprows=skiprows,
+                                  skiprows=self.skiprows,
                                   dtype=str,
-                                  engine=engine)
+                                  engine=self.engine)
 
         # name columns properly
-        logging.debug(f"{filename}: naming columns")
-        dataframe.columns = [str_part_no, str_price]
+        logging.debug(f"{self.filename}: naming columns")
+        dataframe.columns = [self.columns_output_names[0], self.columns_output_names[1]]
 
         # change price strings to floats
-        logging.debug(f"{filename}: changing price to floats")
-        dataframe[str_price] = dataframe[str_price].str.replace(",", ".")
-        dataframe[str_price] = pd.to_numeric(dataframe[str_price])
+        logging.debug(f"{self.filename}: changing price to floats")
+        dataframe[self.columns_output_names[1]] = dataframe[self.columns_output_names[1]].str.replace(",", ".")
+        dataframe[self.columns_output_names[1]] = pd.to_numeric(dataframe[self.columns_output_names[1]])
 
         # clear part_no column from floats (if occur)
-        logging.debug(f"{filename}: clearing part_no column")
-        dataframe[str_part_no] = dataframe[str_part_no].astype(str)
-        dataframe[str_part_no] = dataframe[str_part_no].str.replace(r'[.][0]$', '', regex=True)
+        logging.debug(f"{self.filename}: clearing part_no column")
+        dataframe[self.columns_output_names[0]] = dataframe[self.columns_output_names[0]].astype(str)
+        dataframe[self.columns_output_names[0]] = dataframe[self.columns_output_names[0]].str.replace(r'[.][0]$', '',
+                                                                                                      regex=True)
 
         return dataframe
 
