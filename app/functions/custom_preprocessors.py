515bcbf03074ea8c03d0da77430323091d3015d4
crushyna <crushyna@gmail.com>
2020-10-02 14:36:30 +0200

    preprocessing functions are now classmethods (cleaner code)

--- app/functions/custom_preprocessors.py
+++ app/functions/custom_preprocessors.py
@@ -15,6 +15,14 @@ class CustomPreProcessors:
     OR save file directly (when save_raw = 1).
     """
 
+    partno_list = []
+    ss_list = []
+    price_list = []
+
+    partno_series = pd.Series(dtype=str)
+    ss_series = pd.Series(dtype=str)
+    price_series = pd.Series(dtype=str)
+
     @staticmethod
     def run_custom(country_name: str, make: str, filename: str, country_short: str):
         """
@@ -46,13 +54,10 @@ class CustomPreProcessors:
             typer.echo(message)
             raise typer.Exit()
 
-    @staticmethod
-    def ireland_ford(filename: str):
+    @classmethod
+    def ireland_ford(cls, filename):
         ford_file = filename
         ford_fixed = "ford_ireland_tempfile"
-
-        partno_list = []
-        price_list = []
 
         with open(os.path.join(GlobalSettings.acquisiton_folder, ford_file), 'r') as infile, open(ford_fixed, 'w') as outfile:
             content = infile.read()
@@ -60,17 +65,17 @@ class CustomPreProcessors:
             outfile.write(content_new)
         with open(ford_fixed, 'r') as infile:
             for each_line in infile:
-                partno_list.append(each_line[10:17])
-                price_list.append(each_line[44:53])
+                cls.partno_list.append(each_line[10:17])
+                cls.price_list.append(each_line[44:53])
 
-        price_list = [x if x != '' else '0.00' for x in price_list]
-        price_list = [x.strip() if x != '' else '0.00' for x in price_list]
-        price_list = [0.00 if x == '' else x for x in price_list]
+        cls.price_list = [x if x != '' else '0.00' for x in cls.price_list]
+        cls.price_list = [x.strip() if x != '' else '0.00' for x in cls.price_list]
+        cls.price_list = [0.00 if x == '' else x for x in cls.price_list]
 
-        partno_series = pd.Series(partno_list).astype(str)
-        price_series = pd.Series(price_list).astype(float)
+        cls.partno_series = pd.Series(cls.partno_list).astype(str)
+        cls.price_series = pd.Series(cls.price_list).astype(float)
 
-        dataframe = pd.DataFrame({"part_no": partno_series, "price": price_series})
+        dataframe = pd.DataFrame({"part_no": cls.partno_series, "price": cls.price_series})
         os.remove(ford_fixed)
 
         return dataframe
@@ -87,35 +92,28 @@ class CustomPreProcessors:
             content_new = re.sub("(.{60})", "\\1\n", content, 0, re.DOTALL)
             outfile.write(content_new)
 
-    @staticmethod
-    def ireland_fiat(filename: str):
+    @classmethod
+    def ireland_fiat(cls, filename: str):
         fiat_file = filename
-
-        partno_list = []
-        ss_list = []
-        price_list = []
 
         with open(os.path.join(GlobalSettings.acquisiton_folder, fiat_file), 'r') as infile:
             for each_line in infile:
-                partno_list.append(each_line[0:13])
-                price_list.append(each_line[19:24] + '.' + each_line[24:26])
-                ss_list.append(each_line[105:118])
+                cls.partno_list.append(each_line[0:13])
+                cls.price_list.append(each_line[19:24] + '.' + each_line[24:26])
+                cls.ss_list.append(each_line[105:118])
 
-        partno_series = pd.Series(partno_list).astype(str)
-        ss_series = pd.Series(ss_list).astype(str)
-        price_series = pd.Series(price_list).astype(float)
+        cls.partno_series = pd.Series(cls.partno_list).astype(str)
+        cls.ss_series = pd.Series(cls.ss_list).astype(str)
+        cls.price_series = pd.Series(cls.price_list).astype(float)
 
-        dataframe = pd.DataFrame({"part_no": partno_series, "ss": ss_series, "price": price_series})
+        dataframe = pd.DataFrame({"part_no": cls.partno_series, "ss": cls.ss_series, "price": cls.price_series})
 
         return dataframe
 
-    @staticmethod
-    def australia_porsche(filename: str):
+    @classmethod
+    def australia_porsche(cls, filename: str):
         porsche_file = filename
-
         porsche_fixed = "porsche_australia_tempfile"
-        partno_list = []
-        price_list = []
 
         with open(os.path.join(GlobalSettings.acquisiton_folder, porsche_file), 'r') as infile, open(porsche_fixed, 'w') as outfile:
             content = infile.read()
@@ -124,65 +122,57 @@ class CustomPreProcessors:
 
         with open(porsche_fixed, 'r') as infile:
             for each_line in infile:
-                partno_list.append(each_line[:14])
-                price_list.append(each_line[117:125])
+                cls.partno_list.append(each_line[:14])
+                cls.price_list.append(each_line[117:125])
 
-        partno_series = pd.Series(partno_list).astype(str)
-        price_series = pd.Series(price_list).astype(str)
+        cls.partno_series = pd.Series(cls.partno_list).astype(str)
+        cls.price_series = pd.Series(cls.price_list).astype(str)
 
-        dataframe = pd.DataFrame({"part_no": partno_series, "price": price_series})
+        dataframe = pd.DataFrame({"part_no": cls.partno_series, "price": cls.price_series})
         os.remove(porsche_fixed)
 
         return dataframe
 
-    @staticmethod
-    def australia_toyota(filename: str):
+    @classmethod
+    def australia_toyota(cls, filename: str):
         toyota_file = filename
-
-        partno_list = []
-        ss_list = []
-        price_list = []
 
         with open(os.path.join(GlobalSettings.acquisiton_folder, toyota_file), 'r') as infile:
             for each_line in infile:
-                partno_list.append(each_line[8:20])
-                ss_list.append('' if each_line[362:372].isspace() else f"{each_line[362:372]}     01")
+                cls.partno_list.append(each_line[8:20])
+                cls.ss_list.append('' if each_line[362:372].isspace() else f"{each_line[362:372]}     01")
                 try:
-                    price_list.append(re.search(r"([+].{13})", each_line[73:397])[0])
+                    cls.price_list.append(re.search(r"([+].{13})", each_line[73:397])[0])
                 except TypeError:
                     pass
 
-        partno_series = pd.Series(partno_list).astype(str)
-        ss_series = pd.Series(ss_list).astype(str)
-        price_series = pd.Series(price_list).astype(str)
+        cls.partno_series = pd.Series(cls.partno_list).astype(str)
+        cls.ss_series = pd.Series(cls.ss_list).astype(str)
+        cls.price_series = pd.Series(cls.price_list).astype(str)
 
-        dataframe = pd.DataFrame({"part_no": partno_series, "ss": ss_series, "price": price_series})
+        dataframe = pd.DataFrame({"part_no": cls.partno_series, "ss": cls.ss_series, "price": cls.price_series})
         dataframe = dataframe.iloc[2:][:-1].reset_index(drop=True)
 
         return dataframe
 
-    @staticmethod
-    def australia_kia(filename):
+    @classmethod
+    def australia_kia(cls, filename: str):
         kia_file = filename
-
-        partno_list = []
-        # ss_list = []
-        price_list = []
 
         with open(os.path.join(GlobalSettings.acquisiton_folder, kia_file), 'r') as infile:
             for _ in range(1):
                 next(infile)
             for each_line in infile:
-                partno_list.append(each_line[1:23])
-                price_list.append(each_line[53:61])
+                cls.partno_list.append(each_line[1:23])
+                cls.price_list.append(each_line[53:61])
                 # ss_list.append(each_line[63:64])
 
-        partno_series = pd.Series(partno_list).astype(str)
-        price_series = pd.Series(price_list).astype(float)
+        cls.partno_series = pd.Series(cls.partno_list).astype(str)
+        cls.price_series = pd.Series(cls.price_list).astype(float)
         # ss_series = pd.Series(ss_list).astype(str)
 
         # dataframe = pd.DataFrame({"part_no": partno_series, "price": price_series, "ss": ss_series})
-        dataframe = pd.DataFrame({"part_no": partno_series, "price": price_series})
+        dataframe = pd.DataFrame({"part_no": cls.partno_series, "price": cls.price_series})
 
         # dataframe.ss = dataframe.ss.astype(str).str.strip()
         dataframe.part_no = dataframe.part_no.str.strip()
